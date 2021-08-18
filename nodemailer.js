f3bae9d56c734660a0d46603ee2a367d8a6de671
charlieScript <gozione@gmail.com>
2021-08-18 18:36:04 +0100

    fixed gmail

--- nodemailer.js
+++ nodemailer.js
@@ -2,23 +2,23 @@ const nodemailer = require('nodemailer');
 
 async function main(reciever, message) {
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-
-  // const transporter = nodemailer.createTransport({
-  //   service: 'gmail',
+  // let transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 465,
+  //   secure: true, // true for 465, false for other ports
   //   auth: {
-  //     user: process.env.user,
-  //     pass: process.env.password, // naturally, replace both with your real credentials or an application-specific password
+  //     user: testAccount.user, // generated ethereal user
+  //     pass: testAccount.pass, // generated ethereal password
   //   },
   // });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.user,
+      pass: process.env.password, // naturally, replace both with your real credentials or an application-specific password
+    },
+  });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
