2e816ba246064f9885c9a1aed9b14181bf0a3704
charlieScript <gozione@gmail.com>
2021-08-18 18:48:54 +0100

    fixed gmail

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/form', (req, res) => {
     // message to myself
     nodemailer('onedibecharles19@gmail.com', `this User Contacted You ${email}, ${name}, ${message}`)
     res.status(200).json({
-      message: 'sent'
+      message: process.env.user
     })
   } catch (error) {
     res.json({
